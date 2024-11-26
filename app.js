6caaad2e713d668cf4d4e2a2574083df4284509c
Manikantareddy Sakhamreddy <manikantasakham09@gmail.com>
2024-11-25 19:31:38 -0500

    change1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('My self manikanta and I am bioinformatics and machine learning enthusiast'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
